684f9e5ff8c7781173c3b86977df77d3847ff918
hourong <nmghr9@gmail.com>
2017-03-29 11:14:20 +0800

    修改城市匹配脚本，增加城市国家的 alias 匹配，同时打印两者相似度之和，并排序

--- Airport/get_table.py
+++ Airport/get_table.py
@@ -50,11 +50,31 @@ def get_city_dict():
   city.id         AS ID,
   city.name       AS city,
   city.name_en    AS city_name_en,
-  country.name_en AS country_name_en
+  city.alias      AS city_alias,
+  country.name_en AS country_name_en,
+  country.alias   AS country_alias
 FROM city
   INNER JOIN country ON city.country_id = country.mid;''')
         for line in cursor.fetchall():
-            __city_dict[(line[2], line[3])] = line
+            city_keys = []
+            if line[2] is not None and line[2] != 'NULL' and line[2] != '':
+                city_keys.append(line[2])
+            if line[3] is not None:
+                for c_k in line[3].split('|'):
+                    if c_k != 'NULL' and c_k != '':
+                        city_keys.append(c_k)
+
+            country_keys = []
+            if line[4] is not None and line[4] != 'NULL' and line[4] != '':
+                country_keys.append(line[4])
+            if line[5] is not None:
+                for c_k in line[5].split('|'):
+                    country_keys.append(c_k)
+
+            for country in country_keys:
+                for city in city_keys:
+                    __city_dict[city, country] = [line[0], line[1], line[2], line[4]]
+
     conn.close()
     return __city_dict
 
@@ -68,7 +88,7 @@ if __name__ == '__main__':
     result = pandas.DataFrame(
         columns=['IATA', 'ICAO', 'AirPort name', 'Location served', 'wiki city name', 'wiki country name', 'MapInfo',
                  'map_info',
-                 'ID', 'name', 'name_en', 'country'])
+                 'ID', 'name', 'name_en', 'country', 'total_ratio'])
 
     insert_res = result.T
     _line_count = 0
@@ -80,36 +100,45 @@ if __name__ == '__main__':
             print(_count)
         wiki_city, wiki_country = k
         is_first = True
+        similar_list = []
         for key, value in city_dict.items():
             mioji_city, mioji_country = key
 
             # 相似判断
             try:
-                bool_test = ratio(wiki_city, mioji_city) > 0.8 and ratio(wiki_country, mioji_country) > 0.6
+                city_ratio = ratio(wiki_city, mioji_city)
+                country_ratio = ratio(wiki_country, mioji_country)
+                bool_test = city_ratio > 0.8 and country_ratio > 0.6
+                total_ratio = city_ratio + country_ratio
             except:
                 bool_test = False
+                total_ratio = 0
 
             if bool_test:
-                if is_first:
-                    is_first = False
-                    res = []
-                    res.extend(v)
-                    res.extend(value)
-                    insert_res[_line_count] = res
-                    _line_count += 1
-                else:
-                    res = []
-                    res.extend(['' for i in range(len(v))])
-                    res.extend(value)
-                    insert_res[_line_count] = res
-                    _line_count += 1
+                similar_list.append((value, total_ratio))
 
-        if is_first:
+        similar_list.sort(key=lambda x: x[1], reverse=True)
+        if len(similar_list) == 0:
             res = []
             res.extend(v)
-            res.extend(['' for i in range(4)])
+            res.extend([''] * 5)
             insert_res[_line_count] = res
             _line_count += 1
+        else:
+            id_set = set()
+            for similar in similar_list:
+                if similar[0][0] not in id_set:
+                    res = []
+                    if is_first:
+                        res.extend(v)
+                        is_first = False
+                    else:
+                        res.extend([''] * 8)
+                    res.extend(similar[0])
+                    res.append(similar[1])
+                    id_set.add(similar[0][0])
+                    insert_res[_line_count] = res
+                    _line_count += 1
 
     result_final = insert_res.T
     result_final.to_csv('/Users/hourong/Downloads/test123.csv', encoding="utf8")
