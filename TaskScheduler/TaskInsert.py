e652b7b320dfc076c7ee2c2189c222c24ed599e9
hourong <nmghr9@gmail.com>
2017-06-19 17:22:32 +0800

    增加入任务类

---
+++ TaskScheduler/TaskInsert.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/6/19 下午3:41
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : TaskInsert.py
+# @Software: PyCharm
+
+import hashlib
+import json
+import os
+import pickle
+import re
+
+import pymysql
+import time
+from pymysql.cursors import DictCursor
+
+# 当 task 数积攒到每多少时进行一次插入
+# 当程序退出后也会执行一次插入，无论当前 task 积攒为多少
+
+INSERT_WHEN = 2000
+
+# 入库语句
+
+INSERT_SQL = 'INSERT IGNORE INTO Task (`id`, `worker`, `args`, `task_name`, `priority`) VALUES (%s, %s, %s, %s, %s)'
+
+
+class Task(object):
+    def __init__(self, worker, args, task_name, **kwargs):
+        self.worker = worker
+        self.args = args
+        self.task_name = task_name
+
+        if 'priority' in kwargs:
+            self.priority = kwargs['priority']
+        else:
+            self.priority = '0'
+
+        self.id = self.get_task_id()
+
+    def get_task_id(self):
+        if isinstance(self.args, dict):
+            key_list = sorted([(k, v) for k, v in self.args.items()], key=lambda x: x[0])
+            return hashlib.md5((self.worker + str(key_list)).encode()).hexdigest()
+        else:
+            raise TypeError('错误的 args 类型 < {0} >'.format(type(self.args).__name__))
+
+
+class TaskList(list):
+    def append_task(self, task: Task):
+        self.append((task.id, task.worker, task.args, task.task_name, task.priority))
+
+
+class InsertTask(object):
+    def __init__(self, worker, **kwargs):
+        self.worker = worker
+
+        if 'task_name' in kwargs:
+            self.task_name = kwargs['task_name']
+        else:
+            self.task_name = worker + time.strftime("_%y_%m_%d_%H_%M")
+
+        if 'conn_val' in kwargs:
+            self.conn_val = kwargs['conn_val']
+        else:
+            self.conn_val = {
+                'host': 'localhost',
+                'user': 'hourong',
+                'passwd': 'hourong',
+                'db': 'Task',
+                'charset': 'utf8'
+            }
+
+        self.conn = self.connect()
+        self.tasks = TaskList()
+
+    def connect(self):
+        return pymysql.connect(self.conn_val)
+
+    def insert_into_database(self):
+        cursor = self.conn.cursor()
+        res = cursor.executemany(INSERT_SQL, self.tasks)
+        cursor.close()
+        print('本次准备入库任务数：{0}\n实际入库数（其中有重复任务）：{1}'.format(len(self.tasks), res))
+
+        # 入库完成，清空任务列表
+        self.tasks = TaskList()
+
+    def insert_stat(self):
+        """
+        用于检查当前是否可以准备将任务入到 mysql 中
+        :return: bool 是否准备将任务入到 mysql 中
+        """
+        return len(self.tasks) >= INSERT_WHEN
+
+    def insert_task(self, args):
+        if isinstance(args, dict):
+            __t = Task(self.worker, args, self.task_name)
+            self.tasks.append_task(__t)
+
+            # 如果当前可以入库，则执行入库
+            if self.insert_stat():
+                self.insert_into_database()
+        else:
+            raise TypeError('错误的 args 类型 < {0} >'.format(type(args).__name__))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.insert_into_database()
+
+
+if __name__ == '__main__':
+    pass
---
+++ TaskScheduler/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/6/19 下午3:41
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
