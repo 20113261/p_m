eabd8a304002cda8c2e6752eb9e7694d48217763
hourong <nmghr9@gmail.com>
2017-06-20 16:34:05 +0800

    酒店图片更新流程修改，去除其中的一步，简化更新流程

--- hotel_image/update_hotel_img.py
+++ hotel_image/update_hotel_img.py
@@ -1,4 +1,5 @@
 import pymysql
+import time
 
 SQL_DICT = dict(host='10.10.189.213', user='hourong', passwd='hourong', charset="utf8")
 
@@ -18,33 +19,43 @@ def get_uid_set():
 
 if __name__ == '__main__':
     uid_set = get_uid_set()
+    # uid_set = {'ht20019880'}
     conn = pymysql.connect(db='update_img', **SQL_DICT)
     online_conn = pymysql.connect(db='onlinedb', **SQL_DICT)
     _count = 0
     data = []
-    with online_conn as online_cursor:
-        with conn as cursor:
-            for uid in uid_set:
-                img_list = []
-                online_cursor.execute('select source,sid from hotel_unid where uid=%s', (uid,))
-                for line in online_cursor.fetchall():
-                    cursor.execute('select hotel_jpg from cloud_pic where source=%s and source_id=%s', line)
-                    for line in cursor.fetchall():
-                        img_list.extend(line[0].split('|'))
-                try:
-                    cursor.execute('select pic_md5 from pic_relation where pic_md5 in ({0}) group by file_md5'.format(
-                        ','.join(map(lambda x: '"' + x + '"', img_list))))
-                except:
-                    continue
-                img_result = []
-                for line in cursor.fetchall():
-                    img_result.append(line[0])
-                data.append(('|'.join(img_result), img_result[0], uid))
-                _count += 1
-                if _count % 100 == 0:
-                    online_cursor.executemany('update hotel set img_list=%s, first_img=%s where uid=%s', data)
-                    data = []
-                    print(_count)
-        online_cursor.executemany('update hotel set img_list=%s, first_img=%s where uid=%s', data)
-    conn.close()
-    online_conn.close()
+    for uid in uid_set:
+        online_cursor = online_conn.cursor()
+        cursor = conn.cursor()
+        online_cursor.execute('''SELECT
+  source,
+  sid
+FROM hotel_unid
+WHERE uid = %s;''', (uid,))
+
+        start = time.time()
+        img_result = []
+        for each in online_cursor.fetchall():
+            cursor.execute('''SELECT pic_md5
+FROM pic_relation
+WHERE source = %s AND source_id = %s GROUP BY file_md5;''', each)
+
+            for line in cursor.fetchall():
+                img_result.append(line[0])
+        if img_result:
+            data.append(('|'.join(img_result), img_result[0], uid))
+        cursor.close()
+        online_cursor.close()
+        _count += 1
+        if _count % 100 == 0:
+            online_cursor = online_conn.cursor()
+            online_cursor.executemany('update hotel set img_list=%s, first_img=%s where uid=%s', data)
+            online_cursor.close()
+            data = []
+            print(_count)
+
+    online_cursor = online_conn.cursor()
+    online_cursor.executemany('update hotel set img_list=%s, first_img=%s where uid=%s', data)
+    online_cursor.close()
+    data = []
+    print(_count)
