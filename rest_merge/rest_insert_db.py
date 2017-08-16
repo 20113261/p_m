54f350c764b909665da5d7261e7f763bdf88c7bf
nmghr9 <nmghr9@gmail.com>
2017-08-16 10:11:45 +0800

    rest merge repair

--- rest_merge/rest_insert_db.py
+++ rest_merge/rest_insert_db.py
@@ -2,52 +2,87 @@
 import json
 import gc
 import pymysql
+import toolbox.Common
 from pymysql.cursors import DictCursor
+from collections import defaultdict
 
-json_name_list = ['review_num', 'real_ranking', 'price', 'description']
-norm_name_list = ['name', 'name_en', 'map_info', 'address', 'grade', 'res_url', 'telphone', 'open_time',
-                  'prize', 'traveler_choice', 'price_level', 'cuisines', 'image_urls']
+others_name_list = ['source']
+json_name_list = ['commentcounts', 'ranking', 'price', 'introduction']
+norm_name_list = ['name', 'name_en', 'map_info', 'address', 'grade', 'url', 'phone', 'opentime',
+                  'prize', 'traveler_choice', 'price_level', 'cuisines', 'imgurl']
+
+key_priority = defaultdict(int)
+key_priority.update({
+    'daodao': 10,
+    'qyer': 9,
+    'yelp': 8,
+    'tripadvisor': 10
+})
 
 
-def get_rest_dict(rest_id_list):
+def get_key_by_priority(src: dict):
+    if len(src.keys()) == 0:
+        return None
+    for k, v in sorted(src.items(), key=lambda x: key_priority[x[0]], reverse=True):
+        if toolbox.Common.is_legal(v):
+            return v
+    return None
+
+
+def get_rest_dict(city_id):
+    rest_dict = defaultdict(dict)
+
+    # get s sid
     conn = pymysql.connect(host='10.10.180.145', user='hourong', passwd='hourong', charset='utf8', db='rest_merge')
+    with conn.cursor() as cursor:
+        sql = '''SELECT
+  source,
+  source_id
+FROM rest_unid
+WHERE city_id = %s;'''
+        cursor.execute(sql, (city_id,))
+        res = ["('{0}', '{1}')".format(s, sid) for s, sid in cursor.fetchall()]
+    conn.close()
+
+    # 优化内存占用问题
+    # get whole line in per city
+    conn = pymysql.connect(host='10.10.228.253', user='mioji_admin', passwd='mioji1109', charset='utf8', db='base_data')
     with conn.cursor(cursor=DictCursor) as cursor:
-        rest_dict = {}
-        sql = "select * from rest_merge.rest where id in (%s)" % ','.join(
-            ["\"" + x + "\"" for x in rest_id_list])
+        sql = "select * from rest where (source, id) in (%s)" % ','.join(res)
+
         cursor.execute(sql)
         for line in cursor.fetchall():
-            rest_dict[line['id']] = line
+            source = line['source']
+            source_id = line['id']
+            rest_dict[(source, source_id)] = line
     conn.close()
     return rest_dict
 
 
 def get_task():
     conn = pymysql.connect(host='10.10.180.145', user='hourong', passwd='hourong', charset='utf8', db='rest_merge')
-
     # 获取所有用于融合的城市 id
     cursor = conn.cursor()
-    cursor.execute("select distinct city_id from rest_unid")
+    cursor.execute("select distinct city_id from rest_unid where city_id=50012;")
     total_city_id = list(map(lambda x: x[0], cursor.fetchall()))
     cursor.close()
 
     for each_city_id in total_city_id:
         print("City ID:", each_city_id)
-
         cursor = conn.cursor(cursor=DictCursor)
-        city_rest_dict = {}
-        sql = "select * from rest_merge.rest_unid where city_id=%s"
+        city_rest_dict = defaultdict(list)
+        sql = '''SELECT
+  id,
+  city_id,
+  group_concat(concat(source, '|', source_id) SEPARATOR '|_||_|') AS union_info
+FROM rest_unid WHERE city_id=%s
+GROUP BY id'''
         cursor.execute(sql, (each_city_id,))
         for line in cursor.fetchall():
             miaoji_id = line['id']
-            source = line['source']
-            source_id = line['source_id']
             city_id = line['city_id']
-            city_name = line['city_name']
-            country_name = line['country_name']
-            if city_id not in city_rest_dict:
-                city_rest_dict[city_id] = []
-            city_rest_dict[city_id].append((miaoji_id, source, source_id, city_name, country_name))
+            union_info = line['union_info']
+            city_rest_dict[city_id].append((miaoji_id, city_id, union_info))
         yield city_rest_dict
         cursor.close()
 
@@ -58,54 +93,76 @@ if __name__ == '__main__':
     for task_dict in get_task():
         count = 0
         data = []
-        sql = 'insert ignore into rest_merge.restaurant_tmp(`id`,`name`,`name_en`,`source`,`city_id`,`city`,`map_info`,`address`,`real_ranking`,`grade`,`res_url`,`telphone`,`introduction`,`open_time_desc`,`prize`,`traveler_choice`,`review_num`,`price`,`price_level`,`cuisines`, `image_urls`,`tagid`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-
         # 获取融合城市信息
         for key, values in list(task_dict.items()):
-            rest_info_dict = get_rest_dict([x[2] for x in values])
-            for rest_key_info in values:
-                data_dict = {}
-                miaoji_id = rest_key_info[0]
-                source = rest_key_info[1]
-                source_id = rest_key_info[2]
-                city_name = rest_key_info[3]
-                country_name = rest_key_info[4]
+            # get per city rest info
+            rest_info_dict = get_rest_dict(key)
+            for miaoji_id, city_id, union_info in values:
+                # 初始化融合前变量
+                data_dict = defaultdict(dict)
+                can_be_used = False
 
-                try:
-                    rest_info = rest_info_dict[source_id]
-                except Exception as e:
-                    print(str(e))
+                # 初始化融合信息
+                for each_name in (json_name_list + norm_name_list + others_name_list):
+                    data_dict[each_name] = {}
+
+                # 遍历所有需要融合的 source 以及 id，并生成 dict 类融合内容
+                for s_sid in union_info.split('|_||_|'):
+                    source, source_id = s_sid.split('|')
+
+                    # 未获得融合 id 信息
+                    if not source_id or not source:
+                        continue
+
+                    # 未获得融合数据
+                    rest_info = rest_info_dict[(source, source_id)]
+                    if rest_info == {}:
+                        continue
+
+                    can_be_used = True
+
+                    for each_name in (json_name_list + norm_name_list):
+                        if str(rest_info[each_name]).upper() not in ('NULL', ''):
+                            data_dict[each_name][source] = rest_info[each_name]
+
+                # 不能融合的内容包含两种
+                if not can_be_used:
+                    print('union_info', union_info)
                     continue
 
+                new_data_dict = {}
                 for norm_name in norm_name_list:
-                    val = rest_info[norm_name] or ''
-                    if val != 'NULL':
-                        data_dict[norm_name] = val
-                    else:
-                        data_dict[norm_name] = ''
+                    new_data_dict[norm_name] = get_key_by_priority(data_dict[norm_name]) or ''
 
                 for json_name in json_name_list:
-                    data_dict[json_name] = {}
+                    new_data_dict[json_name] = json.dumps(data_dict[json_name])
 
-                for json_name in json_name_list:
-                    data_dict[json_name][source] = rest_info[json_name] or ''
+                new_data_dict['tagid'] = json.dumps(new_data_dict['cuisines'])
 
-                for json_name in json_name_list:
-                    data_dict[json_name] = json.dumps(data_dict[json_name])
+                new_data_dict['phone'] = new_data_dict['phone'].replace('电话号码：', '').strip()
 
-                data_dict['tagid'] = json.dumps({source: rest_info.get('cuisines', '') or ''})
+                # 添加 source
+                source = '|'.join(sorted(data_dict['source'].values()))
 
-                data_dict['telphone'] = data_dict['telphone'].replace('电话号码：', '').strip()
+                sql = 'insert into chat_restaurant(`id`,`name`,`name_en`,' \
+                      '`source`,`city_id`,`map_info`,`address`,`real_ranking`,' \
+                      '`grade`,`res_url`,`telphone`,`introduction`,`open_time_desc`,`prize`,' \
+                      '`traveler_choice`,`review_num`,`price`,`price_level`,`cuisines`, ' \
+                      '`image_urls`,`tagid`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,' \
+                      '%s,%s,%s,%s,%s,%s,%s,%s)'
+
+                # 替换旧的 data_dict
+                data_dict = new_data_dict
 
                 data.append((
-                    miaoji_id, data_dict['name'], data_dict['name_en'], source, key, city_name,
+                    miaoji_id, data_dict['name'], data_dict['name_en'], source, key,
                     data_dict['map_info'],
                     data_dict['address'],
-                    data_dict['real_ranking'], data_dict['grade'],
-                    data_dict['res_url'], data_dict['telphone'], data_dict['description'], data_dict['open_time'],
+                    data_dict['ranking'], data_dict['grade'],
+                    data_dict['url'], data_dict['phone'], data_dict['introduction'], data_dict['opentime'],
                     data_dict['prize'], data_dict['traveler_choice'],
-                    data_dict['review_num'], data_dict['price'], data_dict['price_level'], data_dict['cuisines'],
-                    data_dict['image_urls'], data_dict['tagid']))
+                    data_dict['commentcounts'], data_dict['price'], data_dict['price_level'], data_dict['cuisines'],
+                    data_dict['imgurl'], data_dict['tagid']))
 
                 if count % 3000 == 0:
                     print("Total:", count)
