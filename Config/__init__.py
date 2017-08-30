5f5c60dbb23825395ac29d6124d3cf0a9cc76f49
nmghr9 <nmghr9@gmail.com>
2017-08-30 11:25:48 +0800

    Poi 融合修改

---
+++ Config/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/29 下午4:13
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Config/settings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/29 下午4:13
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : settings.py
+# @Software: PyCharm
+
+dev_conf = {
+    'host': '10.10.69.170',
+    'user': 'reader',
+    'password': 'miaoji1109',
+    'charset': 'utf8',
+    'db': 'base_data'
+}
+
+attr_merge_conf = {
+    'host': '10.10.180.145',
+    'user': 'hourong',
+    'password': 'hourong',
+    'charset': 'utf8',
+    'db': 'attr_merge'
+}
+
+rest_merge_conf = {
+    'host': '10.10.180.145',
+    'user': 'hourong',
+    'password': 'hourong',
+    'charset': 'utf8',
+    'db': 'rest_merge'
+}
+
+shop_merge_conf = {
+    'host': '10.10.180.145',
+    'user': 'hourong',
+    'password': 'hourong',
+    'charset': 'utf8',
+    'db': 'shop_merge'
+}
