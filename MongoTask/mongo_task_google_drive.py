64c1253116ecb6bfb04df06ebd817f53fbc81c74
nmghr9 <nmghr9@gmail.com>
2017-09-19 19:23:51 +0800

    修改 google 驾车任务

--- MongoTask/mongo_task_google_drive.py
+++ MongoTask/mongo_task_google_drive.py
@@ -17,11 +17,21 @@ client = pymongo.MongoClient(host='10.10.231.105')
 collections = client['MongoTask']['Task']
 
 
+# def task_enter():
+#     _count = 0
+#     for line in open('/tmp/target_url'):
+#         s_l = line.strip().split('###')
+#         yield s_l
+#         _count += 1
+#     print(_count)
+
 def task_enter():
     _count = 0
-    for line in open('/tmp/target_url'):
-        s_l = line.strip().split('###')
-        yield s_l
+    # google_long_drive_url(1)
+    for line in open('/Users/hourong/Downloads/google_long_drive_url(1)'):
+        yield line.strip()
+        # s_l = line.strip().split('###')
+        # yield s_l
         _count += 1
     print(_count)
 
@@ -31,17 +41,17 @@ if __name__ == '__main__':
     _count = 0
     _finished = 0
     exc_set = set()
-    for url, flag, table_name in task_enter():
+    for url in task_enter():
         _count += 1
         task_info = {
             'worker': 'proj.tasks.craw_html',
             'queue': 'hotel_task',
             'routing_key': 'hotel_task',
-            'task_name': 'google_drive_0905',
+            'task_name': 'google_drive_0919',
             'args': {
                 'url': url,
-                'flag': flag,
-                'table_name': table_name
+                'flag': 'google_0919',
+                'table_name': 'new_crawled_html_0919_long'
             },
             'priority': 6,
             'running': 0,
@@ -68,3 +78,49 @@ if __name__ == '__main__':
     print(exc_set)
     print("Now", _count)
     print("Finished", _finished)
+
+
+
+# large task
+
+    # data = []
+    # _count = 0
+    # _finished = 0
+    # exc_set = set()
+    # for url, flag, table_name in task_enter():
+    #     _count += 1
+    #     task_info = {
+    #         'worker': 'proj.tasks.craw_html',
+    #         'queue': 'hotel_task',
+    #         'routing_key': 'hotel_task',
+    #         'task_name': 'google_drive_0905',
+    #         'args': {
+    #             'url': url,
+    #             'flag': flag,
+    #             'table_name': table_name
+    #         },
+    #         'priority': 6,
+    #         'running': 0,
+    #         'used_times': 0,
+    #         'finished': 0,
+    #         'utime': datetime.datetime.now()
+    #     }
+    #     task_info['task_token'] = hashlib.md5(json.dumps(task_info['args'], sort_keys=True).encode()).hexdigest()
+    #     data.append(task_info)
+    #     if _count % 10000 == 0:
+    #         try:
+    #             collections.insert(data, continue_on_error=True)
+    #         except Exception as e:
+    #             pass
+    #         data = []
+    #         print("Now", _count)
+    #         print("Finished", _finished)
+    #
+    # try:
+    #     collections.insert(data, continue_on_error=True)
+    # except Exception as e:
+    #     pass
+    # data = []
+    # print(exc_set)
+    # print("Now", _count)
+    # print("Finished", _finished)
