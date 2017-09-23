d4112a11cdf655907882b4b561a89bff1a3deded
nmghr9 <nmghr9@gmail.com>
2017-09-23 10:28:05 +0800

    修改城市匹配

--- hotel_api/gta_city_info.py
+++ hotel_api/gta_city_info.py
@@ -31,21 +31,26 @@ if __name__ == '__main__':
     }
 
     mioji_similar_dict = Common.MiojiSimilarCityDict.MiojiSimilarCityDict()
-    db = dataset.connect('mysql+pymysql://hourong:hourong@localhost/hotel_api?charset=utf8')
+    db = dataset.connect('mysql+pymysql://user:password@localhost/hotel_api?charset=utf8')
     table = db['gta_city']
 
     for line in table:
         city_id = None
         # 按国家二字码和城市名匹配
         if is_legal(line['country_code']):
-            city_id = mioji_similar_dict.get_mioji_city_id(
+            city_id_set = mioji_similar_dict.get_mioji_city_id(
                 (key_modify(line['country_code']), key_modify(line['city_name'])))
+            # 当前由于对方城市没有提供相应的经纬度，可先由唯一判定，如果提供经纬度，可由经纬度进行距离判定，例如选择最近的，同时距离小于某一数值的城市进行匹配
+            if len(city_id_set) == 1:
+                city_id = city_id_set[0]
 
         # 按国家名和城市名进行匹配
         if city_id is None:
             if is_legal(line['country_name']):
-                city_id = mioji_similar_dict.get_mioji_city_id(
+                city_id_set = mioji_similar_dict.get_mioji_city_id(
                     (key_modify(line['country_name']), key_modify(line['city_name'])))
+                if len(city_id_set) == 1:
+                    city_id = city_id_set[0]
 
         if city_id is not None:
             print(table.update(
