ec12a923af729d9103d7183d2f6f25deda21ed9d
nmghr9 <nmghr9@gmail.com>
2017-10-12 21:32:08 +0800

    例行数据整合，图片整合脚本编写

---
+++ serviceplatform_data/init_img_data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# 使用 country 进行测试，导出 replace sql 数据
+mysqldump -h10.10.69.170 -ureader -pmiaoji1109 --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert base_data country| sed 's/`country`/`test_country`/g'
+
+# 导出酒店旧表
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img pic_relation| sed 's/`pic_relation`/`hotel_images`/g' >  pic_relation.sql
+
+# 导出酒店 0905_old
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img pic_relation_0905_old| sed 's/`pic_relation_0905_old`/`hotel_images`/g' > pic_relation_0905_old.sql
+
+# 导出酒店 0914
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img pic_relation_0914| sed 's/`pic_relation_0914`/`hotel_images`/g' > pic_relation_0914.sql
+
+# 0905
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img pic_relation_0905| sed 's/`pic_relation_0905`/`hotel_images`/g' > pic_relation_0905.sql
+
+# 使用优先级 pic_relation < 0905_old < 0914 < 0905
+
+# POI
+# poi_bucket_relation
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img poi_bucket_relation| sed 's/`poi_bucket_relation`/`poi_images`/g' > poi_bucket_relation.sql
+
+# poi_bucket_relation_0925
+mysqldump -h10.10.189.213 -uhourong -phourong --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert update_img poi_bucket_relation_0925| sed 's/`poi_bucket_relation_0925`/`poi_images`/g' > poi_bucket_relation_0925.sql
---
+++ serviceplatform_data/sql/hotel_images.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `%s` (
+  `id`          BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `source`      VARCHAR(20)  NOT NULL,
+  `source_id`   VARCHAR(64)  NOT NULL DEFAULT '',
+  `pic_url`     TEXT,
+  `pic_md5`     VARCHAR(64)  NOT NULL DEFAULT '',
+  `part`        VARCHAR(10)  NOT NULL DEFAULT '',
+  `hotel_id`    VARCHAR(20)  NOT NULL,
+  `status`      VARCHAR(10)  NOT NULL DEFAULT '-1',
+  `update_date` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `size`        VARCHAR(40)           DEFAULT '',
+  `flag`        VARCHAR(10)           DEFAULT '1',
+  `file_md5`    VARCHAR(32)  NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_sid_md5` (`source`, `source_id`, `file_md5`),
+  KEY `pic_md5` (`pic_md5`),
+  KEY `pic_md5_2` (`pic_md5`, `file_md5`),
+  KEY `flag` (`flag`),
+  KEY `update_date` (`update_date`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
