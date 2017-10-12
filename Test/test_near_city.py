2707b2a00f96cf0f5bfe7d28fb420e5e01c3690f
nmghr9 <nmghr9@gmail.com>
2017-10-12 13:18:48 +0800

    add test case

--- Test/test_near_city.py
+++ Test/test_near_city.py
@@ -5,3 +5,10 @@
 # @Site    : 
 # @File    : test_near_city.py
 # @Software: PyCharm
+from get_near_city.get_near_city import get_nearby_city
+
+if __name__ == '__main__':
+    city_id = '51513'
+    map_info = '57.581802,-20.008942'
+    nearby_city = get_nearby_city(city_id, map_info)
+    print(nearby_city)
