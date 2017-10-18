61721cb0eacc86477fad8d85fee219798d0a9ca1
nmghr9 <nmghr9@gmail.com>
2017-10-18 09:21:45 +0800

    增加 google 驾车任务脚本

--- MongoTask/mongo_task_google_drive.py
+++ MongoTask/mongo_task_google_drive.py
@@ -17,23 +17,24 @@ client = pymongo.MongoClient(host='10.10.231.105')
 collections = client['MongoTask']['Task']
 
 
-# def task_enter():
-#     _count = 0
-#     for line in open('/tmp/target_url'):
-#         s_l = line.strip().split('###')
-#         yield s_l
-#         _count += 1
-#     print(_count)
-
 def task_enter():
     _count = 0
-    # google_long_drive_url(1)
-    for line in open('/Users/hourong/Downloads/google_long_drive_url(1)'):
-        yield line.strip()
-        # s_l = line.strip().split('###')
-        # yield s_l
+    for line in open('/tmp/target_url'):
+        s_l = line.strip().split('###')
+        yield s_l
         _count += 1
     print(_count)
+
+
+# def task_enter():
+#     _count = 0
+#     # google_long_drive_url(1)
+#     for line in open('/Users/hourong/Downloads/google_long_drive_url(1)'):
+#         yield line.strip()
+#         # s_l = line.strip().split('###')
+#         # yield s_l
+#         _count += 1
+#     print(_count)
 
 
 if __name__ == '__main__':
@@ -41,17 +42,17 @@ if __name__ == '__main__':
     _count = 0
     _finished = 0
     exc_set = set()
-    for url in task_enter():
+    for url, flag, table_name in task_enter():
         _count += 1
         task_info = {
             'worker': 'proj.tasks.craw_html',
             'queue': 'hotel_task',
             'routing_key': 'hotel_task',
-            'task_name': 'google_drive_0919',
+            'task_name': 'google_drive_1018',
             'args': {
                 'url': url,
-                'flag': 'google_0919',
-                'table_name': 'new_crawled_html_0919_long'
+                'flag': flag,
+                'table_name': table_name
             },
             'priority': 6,
             'running': 0,
@@ -61,7 +62,7 @@ if __name__ == '__main__':
         }
         task_info['task_token'] = hashlib.md5(json.dumps(task_info['args'], sort_keys=True).encode()).hexdigest()
         data.append(task_info)
-        if _count % 10000 == 0:
+        if _count % 1000 == 0:
             try:
                 collections.insert(data, continue_on_error=True)
             except Exception as e:
@@ -83,44 +84,44 @@ if __name__ == '__main__':
 
 # large task
 
-    # data = []
-    # _count = 0
-    # _finished = 0
-    # exc_set = set()
-    # for url, flag, table_name in task_enter():
-    #     _count += 1
-    #     task_info = {
-    #         'worker': 'proj.tasks.craw_html',
-    #         'queue': 'hotel_task',
-    #         'routing_key': 'hotel_task',
-    #         'task_name': 'google_drive_0905',
-    #         'args': {
-    #             'url': url,
-    #             'flag': flag,
-    #             'table_name': table_name
-    #         },
-    #         'priority': 6,
-    #         'running': 0,
-    #         'used_times': 0,
-    #         'finished': 0,
-    #         'utime': datetime.datetime.now()
-    #     }
-    #     task_info['task_token'] = hashlib.md5(json.dumps(task_info['args'], sort_keys=True).encode()).hexdigest()
-    #     data.append(task_info)
-    #     if _count % 10000 == 0:
-    #         try:
-    #             collections.insert(data, continue_on_error=True)
-    #         except Exception as e:
-    #             pass
-    #         data = []
-    #         print("Now", _count)
-    #         print("Finished", _finished)
-    #
-    # try:
-    #     collections.insert(data, continue_on_error=True)
-    # except Exception as e:
-    #     pass
-    # data = []
-    # print(exc_set)
-    # print("Now", _count)
-    # print("Finished", _finished)
+# data = []
+# _count = 0
+# _finished = 0
+# exc_set = set()
+# for url, flag, table_name in task_enter():
+#     _count += 1
+#     task_info = {
+#         'worker': 'proj.tasks.craw_html',
+#         'queue': 'hotel_task',
+#         'routing_key': 'hotel_task',
+#         'task_name': 'google_drive_0905',
+#         'args': {
+#             'url': url,
+#             'flag': flag,
+#             'table_name': table_name
+#         },
+#         'priority': 6,
+#         'running': 0,
+#         'used_times': 0,
+#         'finished': 0,
+#         'utime': datetime.datetime.now()
+#     }
+#     task_info['task_token'] = hashlib.md5(json.dumps(task_info['args'], sort_keys=True).encode()).hexdigest()
+#     data.append(task_info)
+#     if _count % 10000 == 0:
+#         try:
+#             collections.insert(data, continue_on_error=True)
+#         except Exception as e:
+#             pass
+#         data = []
+#         print("Now", _count)
+#         print("Finished", _finished)
+#
+# try:
+#     collections.insert(data, continue_on_error=True)
+# except Exception as e:
+#     pass
+# data = []
+# print(exc_set)
+# print("Now", _count)
+# print("Finished", _finished)
---
+++ MongoTask/prepare_google_drive_task.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/18 上午8:58
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : prepare_google_drive_task.py
+# @Software: PyCharm
+import os
+
+if __name__ == '__main__':
+    # ---- Variables ----
+
+    # file_path = '/root/data/task/traffic_total/url/traffic_poi'
+    # file_path = '/root/data/task/0428'
+    # dsc_file_name = '/root/data/task/traffic_poi'
+    # dsc_file_name = '/root/data/task/google_task_0428'
+
+    file_path = '/root/data/task/1018'
+    dsc_file_name = '/root/data/task/1018/target_url'
+    max_line_count = 500000
+
+    # -------------------
+    __num_count = 0
+    table_index = 450
+    table_name = 'new_crawled_html_%03d' % table_index
+    line_count = 0
+    dsc_file = open(dsc_file_name, 'w')
+    for file_name in os.listdir(file_path):
+        file_path_name = os.path.join(file_path, file_name)
+        for line in open(file_path_name):
+            __num_count += 1
+            if __num_count % 10000 == 0:
+                print(__num_count)
+            line_count += 1
+            data = (line.strip(), file_name, table_name)
+            dsc_file.write('###'.join(data) + '\n')
+            if line_count == max_line_count:
+                line_count = 0
+                table_index += 1
+                table_name = 'new_crawled_html_%02d' % table_index
