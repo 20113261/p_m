a4c763c53dc3e1b5095f84fb2e586c6852f5e1cb
nmghr9 <nmghr9@gmail.com>
2017-10-27 14:23:01 +0800

    新增 merge report

--- poi_ori/merge_report.py
+++ poi_ori/merge_report.py
@@ -1,163 +1,202 @@
-# encoding=utf8
-import pandas
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/27 上午11:59
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : merge_report.py
+# @Software: PyCharm
 import datetime
-from pymysql.cursors import DictCursor
-from service_platform_conn_pool import base_data_pool, poi_ori_pool
+import json
+import dataset
+from service_platform_conn_pool import base_data_pool
+from data_source import MysqlSource
 from logger import get_logger
+from collections import defaultdict
+from toolbox.Common import is_legal
 
-logger = get_logger("merge_report")
+logger = get_logger("merge_report_old")
 table_name = None
 poi_name = None
 
-
-def init_global_name(poi_type):
-    global table_name
-    global poi_name
-
-    if poi_type == 'attr':
-        poi_name = '景点'
-        table_name = 'chat_attraction'
-    elif poi_type == 'shop':
-        poi_name = '购物'
-        table_name = 'chat_shopping'
-    elif poi_type == 'rest':
-        poi_name = '餐厅'
-        table_name = 'chat_restaurant'
-    else:
-        raise TypeError("Unknown Type: {}".format(poi_type))
+poi_ori_config = {
+    'host': '10.10.228.253',
+    'user': 'mioji_admin',
+    'passwd': 'mioji1109',
+    'db': 'poi_merge'
+}
 
 
-def init_city_dict():
+def prepare_city_info():
     conn = base_data_pool.connection()
     cursor = conn.cursor()
-    cursor.execute('''SELECT *
-FROM (SELECT
-        id,
-        city.name      AS name,
-        city.name_en   AS name_en,
-        country.name   AS country,
-        CASE WHEN grade = -1
-          THEN 100
-        ELSE grade END AS grade
-      FROM city
-        JOIN country ON city.country_id = country.mid
-      ORDER BY grade) t
-ORDER BY t.grade;''')
-    for line in cursor.fetchall():
-        # id, name, name_en, country, grade
-        yield line
+    cursor.execute('''SELECT
+  id,
+  CASE WHEN grade != -1
+    THEN grade
+  ELSE 100 END AS grade
+FROM city;''')
+    _res = {line[0]: line[1] for line in cursor.fetchall()}
+
+    cursor.execute('''SELECT
+  CASE WHEN grade = -1
+    THEN 100
+  ELSE grade END,
+  count(*)
+FROM city
+GROUP BY grade;''')
+    _grade_info = {line[0]: line[1] for line in cursor.fetchall()}
     cursor.close()
     conn.close()
+    return _res, _grade_info
 
 
 def poi_merged_report(poi_type):
-    init_global_name(poi_type)
+    cid2grade, grade_info = prepare_city_info()
+    if poi_type == 'attr':
+        query_sql = '''SELECT
+      id,
+      city_id,
+      first_image,
+      address,
+      open,
+      introduction,
+      data_source,
+      status_online,
+      utime
+    FROM chat_attraction;'''
+    elif poi_type == 'shop':
+        query_sql = '''SELECT
+              id,
+              city_id,
+              first_image,
+              address,
+              open,
+              introduction,
+              data_source,
+              status_online,
+              utime
+            FROM chat_shopping;'''
+    else:
+        query_sql = '''SELECT
+              id,
+              city_id,
+              first_image,
+              address,
+              open,
+              introduction,
+              data_source,
+              status_online,
+              utime
+            FROM chat_restaurant;'''
 
-    conn = poi_ori_pool.connection()
-    cursor = conn.cursor(cursor=DictCursor)
+    poi_info = defaultdict(dict)
+    for line in MysqlSource(db_config=poi_ori_config, table_or_query=query_sql, size=10000, is_dict_cursor=True,
+                            is_table=False):
+        cid = line['city_id']
 
-    get_column_sql = "SELECT column_name FROM information_schema.columns WHERE table_schema = DATABASE() AND table_name='{}'".format(
-        table_name)
-    cursor.execute(get_column_sql)
+        # get grade
+        grade = cid2grade.get(cid, None)
 
-    datas = cursor.fetchall()
+        if grade is None:
+            # not known cid
+            continue
 
-    column_list = []
+        # add cid
+        if 'has_poi' not in poi_info[grade]:
+            poi_info[grade]['has_poi'] = set()
+        poi_info[grade]['has_poi'].add(line['city_id'])
 
-    filter_info = []
-    error_data_id = []
+        # poi total
+        if 'total' not in poi_info[grade]:
+            poi_info[grade]['total'] = 0
+        poi_info[grade]['total'] += 1
 
-    for data in datas:
-        column_list.append(data['column_name'])
+        # poi online
+        if 'online' not in poi_info[grade]:
+            poi_info[grade]['online'] = 0
+        if 'Open' == line['status_online']:
+            poi_info[grade]['online'] += 1
 
-    report_data = pandas.DataFrame(
-        columns=['ID', '城市名', '国家', '城市 grade', '开发库中{}个数'.format(poi_name), 'test 上线{}个数'.format(poi_name),
-                 '上线{}占比'.format(poi_name), '上线{}有图占比'.format(poi_name), '开发库中 daodao {}个数'.format(poi_name),
-                 '上线 daodao 个数', '上线 daodao {}占比'.format(poi_name)])
-    report_count = 0
-    for cid, city_name, city_name_en, country, grade in init_city_dict():
-        get_sql = "select * from {} where city_id = '{}'".format(table_name, cid)
-        cursor.execute(get_sql)
-        datas = cursor.fetchall()
-        name_null_fail_count = 0
-        city_null_fail_count = 0
-        first_img_null_fail_count = 0
-        norm_tag_null_fail_count = 0
-        map_fail_count = 0
+        # poi update this time
+        if 'update' not in poi_info[grade]:
+            poi_info[grade]['update'] = 0
+        if line['utime'] <= datetime.datetime.now() - datetime.timedelta(days=30):
+            poi_info[grade]['update'] += 1
 
-        img_succeed_count = 0
-        daodao_count = 0
-        daodao_succeed_count = 0
-        success_count = 0
-        for data in datas:
-            # daodao 统计部分
-            if 'daodao' in data['data_source']:
-                daodao_count += 1
+        # poi has img
+        if 'img' not in poi_info[grade]:
+            poi_info[grade]['img'] = 0
+        if is_legal(line['first_image']):
+            poi_info[grade]['img'] += 1
 
-            # 数据检查部分
+        # poi has address
+        if 'address' not in poi_info[grade]:
+            poi_info[grade]['address'] = 0
+        if is_legal(line['address']):
+            poi_info[grade]['address'] += 1
 
-            # name
-            if data['name'].lower() in ('', 'null', '0') and data['name_en'] in ('', 'null', '0'):
-                name_null_fail_count += 1
-                error_data_id.append(data['id'])
-                continue
+        # poi opentime
+        if 'opentime' not in poi_info[grade]:
+            poi_info[grade]['opentime'] = 0
+        if is_legal(line['open']):
+            poi_info[grade]['opentime'] += 1
 
-            # city
-            if data['city_id'] in ('', 'null', '0'):
-                city_null_fail_count += 1
-                error_data_id.append(data['id'])
-                continue
-
-            # map_info
+        # poi introduction
+        if 'introduction' not in poi_info[grade]:
+            poi_info[grade]['introduction'] = 0
+        if is_legal(line['introduction']):
             try:
-                lat = float(data['map_info'].strip().split(',')[0])
-                lgt = float(data['map_info'].strip().split(',')[1])
+                _data = json.loads(line['introduction'])
+                if isinstance(_data, dict):
+                    if _data.values():
+                        if is_legal(_data.values()[0]):
+                            poi_info[grade]['introduction'] += 1
             except Exception:
-                map_fail_count += 1
-                error_data_id.append(data['id'])
-                continue
+                pass
 
-            if 'daodao' in data['data_source']:
-                daodao_succeed_count += 1
+        # qyer\daodao\multi in source
+        if 'qyer' not in poi_info[grade]:
+            poi_info[grade]['qyer'] = 0
+        if 'daodao' not in poi_info[grade]:
+            poi_info[grade]['daodao'] = 0
+        if 'multi' not in poi_info[grade]:
+            poi_info[grade]['multi'] = 0
 
-            success_count += 1
+        if is_legal(line['data_source']):
+            if 'qyer' in line['data_source']:
+                poi_info[grade]['qyer'] += 1
+            if 'daodao' in line['data_source'] or 'tripadvisor' in line['data_source']:
+                poi_info[grade]['daodao'] += 1
+            if 'qyer' in line['data_source'] and (
+                            'daodao' in line['data_source'] or 'tripadvisor' in line['data_source']):
+                poi_info[grade]['multi'] += 1
 
-        if success_count != len(datas):
-            filter_info.append((cid, city_name, country))
+    for each_grade in poi_info.keys():
+        poi_info[each_grade]['has_poi'] = len(poi_info[each_grade]['has_poi'])
 
-        report_data.loc[report_count] = [
-            cid,
-            city_name,
-            country,
-            grade,
-            len(datas),
-            success_count,
-            "{0:04f}%".format(100 * success_count / float(len(datas))) if len(datas) != 0 else '无穷大',
-            "{0:04f}%".format(100 * img_succeed_count / float(success_count)) if success_count != 0 else '无穷大',
-            daodao_count,
-            daodao_succeed_count,
-            "{0:04f}%".format(
-                100 * daodao_succeed_count / float(daodao_count)) if daodao_count != 0 else '无穷大'
-        ]
-        report_count += 1
-        logger.debug(
-            ' '.join([cid, city_name, city_name_en, country, grade]) + '\tall:' + str(len(datas)) + "\tget:" + str(
-                success_count) + '\tname_null:' + str(
-                name_null_fail_count) + '\tcity_null:' + str(city_null_fail_count) + '\timg_null:' + str(
-                first_img_null_fail_count) + '\tmap:' + str(map_fail_count) + '\tnorm_tag:' + str(
-                norm_tag_null_fail_count))
-
-    for each in filter_info:
-        logger.debug("[data_filter: {}]".format(each))
-
-    logger.debug("[error data][id: {}]".format(error_data_id))
-
-    pandas.DataFrame(report_data).to_excel(datetime.datetime.now().strftime('{}_report_%Y_%m_%d.xlsx'.format(poi_name)))
-    pandas.DataFrame(report_data).to_csv(datetime.datetime.now().strftime('{}_report_%Y_%m_%d.csv'.format(poi_name)))
+    db = dataset.connect('mysql+pymysql://mioji_admin:mioji1109@10.10.228.253/Report?charset=utf8')
+    _table = db['base_data_report_summary']
+    dt = datetime.datetime.now()
+    for k, v in sorted(grade_info.items(), key=lambda x: x[0]):
+        _tmp_grade_report = poi_info.get(k, defaultdict(int))
+        no_poi = v - _tmp_grade_report['has_poi']
+        data = {
+            'type': poi_type,
+            'grade': k,
+            'citys': v,
+            'no_poi': no_poi,
+            'date': datetime.datetime.strftime(dt, '%Y%m%d'),
+            'hour': datetime.datetime.strftime(dt, '%H'),
+            'datetime': datetime.datetime.strftime(dt, '%Y%m%d%H00')
+        }
+        data.update(_tmp_grade_report)
+        _table.upsert(data, keys=['type', 'grade', 'datetime'])
+    db.commit()
 
 
 if __name__ == '__main__':
-    import sys
-
-    poi_type = sys.argv[1]
-    poi_merged_report(poi_type)
+    # import sys
+    #
+    # poi_type = sys.argv[1]
+    # poi_merged_report(poi_type)
+    poi_merged_report('attr')
