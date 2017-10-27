b22862441def2c7342adbbf0c3c6a4ca337e90fe
nmghr9 <nmghr9@gmail.com>
2017-10-27 16:27:58 +0800

    增加 tagB 字段处理

--- poi_ori/update_tag_id.py
+++ poi_ori/update_tag_id.py
@@ -8,30 +8,40 @@
 from toolbox.Common import is_legal
 from service_platform_conn_pool import poi_ori_pool, base_data_pool
 from logger import get_logger
+from collections import defaultdict
 
 logger = get_logger("update_tag_id")
 
 poi_type = None
 tag_s = None
+tag_b = None
 task_table = None
+id2tag = {}
 
 
 def init_global_name(_poi_type):
     global poi_type
     global tag_s
+    global tag_b
     global task_table
+    global id2tag
     poi_type = _poi_type
     if poi_type == 'attr':
         tag_s = 'chat_attraction_tagS'
+        tag_b = 'chat_attraction_tagB'
         task_table = 'chat_attraction'
     elif poi_type == 'shop':
         tag_s = 'chat_shopping_tagS'
+        tag_b = 'chat_attraction_tagB'
         task_table = 'chat_shopping'
     elif poi_type == 'rest':
         tag_s = 'chat_restaurant_tagS'
+        tag_b = 'chat_attraction_tagB'
         task_table = 'chat_restaurant'
     else:
         raise TypeError("Unknown Type: {}".format(poi_type))
+
+    id2tag = init_id2tag()
 
 
 def tag2id():
@@ -50,6 +60,44 @@ FROM {};'''.format(tag_s)
     return _dict
 
 
+def init_id2tag():
+    conn = base_data_pool.connection()
+    sql = '''SELECT
+  id,
+  Stag
+FROM {};'''.format(tag_b)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    _dict = defaultdict(set)
+    for _id, _l_s_tag in cursor.fetchall():
+        for each in _l_s_tag.split('|'):
+            if is_legal(each):
+                _dict[_id].add(each)
+    cursor.close()
+    conn.close()
+    return _dict
+
+
+def get_tag(tag_id):
+    b_tag_set = set()
+    for each in tag_id.split('|'):
+        for b_tag, s_tags in id2tag.items():
+            if each in s_tags:
+                b_tag_set.add(int(b_tag[2:]))
+
+    _l_tag = []
+    if poi_type == 'attr':
+        _tag_length = 10
+    elif poi_type == 'shop':
+        _tag_length = 3
+    for i in range(_tag_length):
+        if i in b_tag_set:
+            _l_tag.append('1')
+        else:
+            _l_tag.append('0')
+    return ''.join(_l_tag)
+
+
 def update_each_tag_id():
     tag_id = tag2id()
     conn = poi_ori_pool.connection()
@@ -58,17 +106,27 @@ def update_each_tag_id():
 FROM {};'''.format(
         task_table))
     data = []
+    _count = 0
     for _id, _tag_id in cursor.fetchall():
         if is_legal(_tag_id):
             tag_id_set = set()
             for each in _tag_id.split('|'):
                 tag_id_set.add(tag_id.get(each))
-            data.append(('|'.join(filter(lambda x: x is not None, tag_id_set)), _id))
-    res = cursor.executemany('update base_data.{} set tag_id=%s where id=%s'.format(task_table), data)
+            small_tag = ('|'.join(filter(lambda x: is_legal(x), tag_id_set)))
+            big_tag = get_tag(small_tag)
+            data.append((small_tag, big_tag, _id))
+            _count += 1
+            if len(data) % 1000 == 0:
+                logger.debug("[mk data][poi_type: {}][len: {}]".format(poi_type, _count))
+                res = cursor.executemany('update base_data.{} set tag_id=%s, tagB=%s where id=%s'.format(task_table),
+                                         data)
+                data = []
+                logger.debug("[update tag id][table_name: {}][update count: {}]".format(task_table, res))
+
+    logger.debug("[mk data finished][poi_type: {}][len: {}]".format(poi_type, _count))
     conn.commit()
     cursor.close()
     conn.close()
-    logger.debug("[update tag id][table_name: {}][update count: {}]".format(task_table, res))
 
 
 def update_tag_id(_poi_type):
@@ -77,4 +135,4 @@ def update_tag_id(_poi_type):
 
 
 if __name__ == '__main__':
-    update_tag_id('attr')
+    update_tag_id('shop')
