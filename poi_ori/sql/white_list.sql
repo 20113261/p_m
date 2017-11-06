14bb61ca6e5268dd384ca8e7a6c1ed2653eab302
nmghr9 <nmghr9@gmail.com>
2017-11-06 12:20:06 +0800

    update white list sql

--- poi_ori/sql/white_list.sql
+++ poi_ori/sql/white_list.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `white_list` (
   `id`    INT(11)     NOT NULL AUTO_INCREMENT,
+  `type`  VARCHAR(64) NOT NULL,
   `md5`   VARCHAR(64) NOT NULL,
   `info`  JSON,
   `utime` TIMESTAMP(6)         DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`md5`)
+  UNIQUE KEY `_unique_ix_md5` (`md5`),
+  KEY `_ix_type` (`type`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
