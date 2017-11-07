52dd104dd846e0fc5d62e8bd4fafd21610368dbf
nmghr9 <nmghr9@gmail.com>
2017-11-07 18:33:51 +0800

    未进行人脸识别图片过滤脚本

---
+++ report/poi_img_unscanned_report.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/7 下午3:52
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : poi_img_unscanned_report.py
+# @Software: PyCharm
+
+"""
+set 1: poi_images online attr_bucket use 1 has url
+set 2: PoiPicInformation poi_id no img
+"""
+from data_source import MysqlSource
+from service_platform_conn_pool import poi_face_detect_pool
+from logger import get_logger
+
+logger = get_logger("poi_img_not_scanned_report")
+
+poi_ori_config = {
+    'host': '10.10.228.253',
+    'user': 'mioji_admin',
+    'password': 'mioji1109',
+    'charset': 'utf8',
+    'db': 'BaseDataFinal'
+}
+
+
+def get_poi_pic_detect(poi_id):
+    conn = poi_face_detect_pool.connection()
+    cursor = conn.cursor()
+    query_sql = '''SELECT pic_name
+FROM PoiPictureInformation
+WHERE poi_id = '{}';'''.format(poi_id)
+    cursor.execute(query_sql)
+    res = set([line[0].split('/')[-1] for line in cursor.fetchall()])
+    cursor.close()
+    conn.close()
+    return res
+
+
+def task():
+    query_sql = '''SELECT
+  sid,
+  file_name,
+  bucket_name
+FROM poi_images
+WHERE source = 'online' AND bucket_name LIKE '%attr%' AND sid LIKE 'v%' AND `use` = 1
+ORDER BY sid;'''
+
+    old_poi_id = None
+    img_name_set = set()
+    _count = 0
+
+    result_f = open('/tmp/img_res', mode='w')
+
+    for line in MysqlSource(poi_ori_config, table_or_query=query_sql,
+                            size=10000, is_table=False,
+                            is_dict_cursor=True):
+        _count += 1
+        if _count % 3000 == 0:
+            logger.debug("[now count: {}]".format(_count))
+
+        if 'attr' not in line['bucket_name'] and not line['sid'].startswith('v'):
+            continue
+
+        poi_id = line['sid']
+        file_name = line['file_name']
+        img_name_set.add(file_name)
+
+        if poi_id != old_poi_id:
+            if old_poi_id is not None:
+                has_detected_pic_file = get_poi_pic_detect(poi_id)
+                lost_img = (img_name_set - has_detected_pic_file)
+                for i in lost_img:
+                    logger.debug("[img not detected][poi_id: {}][img: {}]".format(poi_id, i))
+                    result_f.write('{}###{}\n'.format(poi_id, i))
+            old_poi_id = poi_id
+            img_name_set = set()
+
+
+if __name__ == '__main__':
+    task()
---
+++ serviceplatform_data/insert_poi_detect_task_info_tmp_from_file.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/25 上午9:49
+# @Author  : Hou Rong
+# @Site    : 
+# @File    : insert_poi_detect_task_info.py
+# @Software: PyCharm
+import pymysql
+from warnings import filterwarnings
+from service_platform_conn_pool import service_platform_pool, base_data_pool
+from logger import get_logger
+
+filterwarnings('ignore', category=pymysql.err.Warning)
+
+logger = get_logger("insert_poi_detect_task_info_from_img")
+
+service_platform_conf = {
+    'host': '10.10.228.253',
+    'user': 'mioji_admin',
+    'password': 'mioji1109',
+    'charset': 'utf8',
+    'db': 'ServicePlatform'
+}
+
+offset = 0
+cid2grade = None
+
+
+def insert_task_data(data, _count):
+    # 插入 pic detect task 数据
+    insert_sql = '''INSERT IGNORE INTO pic_detect_task_test (city_id, city_grade, poi_id, pic_name) VALUES (%s, %s, %s, %s);'''
+
+    max_retry_times = 3
+    while max_retry_times:
+        max_retry_times -= 1
+        try:
+            conn = service_platform_pool.connection()
+            cursor = conn.cursor()
+            _insert_count = cursor.executemany(insert_sql, data)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            logger.debug(
+                "[insert data][now count: {}][insert data: {}][insert_ignore_count: {}]".format(_count, len(data),
+                                                                                                _insert_count))
+            break
+        except Exception as exc:
+            logger.exception(msg="[run sql error]", exc_info=exc)
+
+
+def prepare_city_info():
+    conn = base_data_pool.connection()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT
+  id,
+  CASE WHEN grade != -1
+    THEN grade
+  ELSE 100 END AS grade
+FROM city;''')
+    _res = {line[0]: line[1] for line in cursor.fetchall()}
+    cursor.close()
+    conn.close()
+    return _res
+
+
+def prepare_poi_info():
+    _dict = {}
+    conn = base_data_pool.connection()
+    cursor = conn.cursor()
+    _sql = '''SELECT
+  id,
+  city_id
+FROM chat_attraction;'''
+    cursor.execute(_sql)
+    _count = 0
+    for line in cursor.fetchall():
+        _count += 1
+        if _count % 10000 == 0:
+            logger.debug("[prepare_pic_task: {}]".format(_count))
+        _dict[line[0]] = line[1]
+    cursor.close()
+    conn.close()
+    logger.debug("[prepare_pic_task: {}]".format(_count))
+    return _dict
+
+
+def get_info():
+    _count = 0
+    data = []
+    f = open('/tmp/img_res')
+    poi2cid = prepare_poi_info()
+    cid2grade = prepare_city_info()
+    _count += 1
+    for line in f:
+        l_task = line.strip().split('###')
+        if len(l_task) != 2:
+            logger.debug("[unknown task][task: {}]".format(line.strip()))
+            continue
+        poi_id, file_name = l_task
+        cid = poi2cid.get(poi_id, '')
+        if not cid:
+            logger.debug("[unknown poi city][poi_id: {}]".format(poi_id))
+            continue
+        c_grade = cid2grade.get(cid, '')
+        if not c_grade:
+            logger.debug("[unknown city grade][poi_id: {}][city_id: {}]".format(poi_id, cid))
+            continue
+        data.append((c_grade, cid, poi_id, file_name))
+
+        if len(data) == 2000:
+            insert_task_data(data, _count)
+            data = []
+    if data:
+        insert_task_data(data, _count)
+
+
+if __name__ == '__main__':
+    get_info()
