02460b1182949b615920a5da2ce4f74eda252601
nmghr9 <nmghr9@gmail.com>
2017-12-12 10:01:19 +0800

    1、添加 baidu sugg 任务 2、qyer sugg 添加更多的搜索 key

--- MongoTask/mongo_task_baidu_city_query.py
+++ MongoTask/mongo_task_baidu_city_query.py
@@ -5,10 +5,9 @@
 # @Site    : 
 # @File    : mongo_task_qyer_city_sugg.py
 # @Software: PyCharm
-from data_source import MysqlSource
-from logger import get_logger
 from MongoTask.MongoTaskInsert import InsertTask, TaskType
-from service_platform_conn_pool import base_data_pool, source_info_pool, fetchall, verify_info_new_pool
+from logger import get_logger
+from service_platform_conn_pool import verify_info_new_pool, fetchall
 
 logger = get_logger("insert_mongo_task")
 
@@ -45,11 +44,12 @@ WHERE s_region REGEXP '[\\u0391-\\uFFE5]' = 0;'''
 
 
 if __name__ == '__main__':
-    with InsertTask(worker='proj.total_tasks.qyer_city_task', queue='supplement_field', routine_key='supplement_field',
-                    task_name='qyer_city_info_20171211a', source='Qyer', _type='CityInfo',
+    with InsertTask(worker='proj.total_tasks.baidu_search_task', queue='supplement_field',
+                    routine_key='supplement_field',
+                    task_name='baidu_search_20171211a', source='Baidu', _type='CityInfo',
                     priority=3, task_type=TaskType.NORMAL) as it:
         for c_name in get_tasks():
             args = {
-                'keyword': c_name
+                'keyword': 'site:place.qyer.com {}'.format(c_name)
             }
             it.insert_task(args)
