57d0b514b5c77ea56905f7116544f8caf99a8056
nmghr9 <nmghr9@gmail.com>
2017-12-27 16:13:45 +0800

    新入库未匹配人脸数据，新 content 任务

--- serviceplatform_data/init_data.sh
+++ serviceplatform_data/init_data.sh
@@ -98,3 +98,5 @@ mysqldump -h10.10.213.148 -umioji_admin -pmioji1109 --insert-ignore --skip-lock-
 # qyer 图片数据合并
 mysqldump -h10.10.228.253 -umioji_admin -pmioji1109 --replace --skip-lock-tables --no-create-info --no-create-db --complete-insert --allow-keywords "" ServicePlatform  |sed 's/`images_total_qyer_20171120a`/`poi_images`/g' > images_total_qyer_20171120a.sql
 
+mysqldump -h10.19.153.98 -umioji_admin -pmioji1109 --insert-ignore --skip-lock-tables --no-create-info --no-create-db ServicePlatform workload_train > workload_train_1226.sql
+
