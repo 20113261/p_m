354f1d59b80592892e8497ebe5dfc208da878951
miojilx <2839255227@qq.com>
2018-01-23 16:28:29 +0800

    代码整合

--- city/city_map_ciytName.py
+++ city/city_map_ciytName.py
@@ -6,6 +6,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 import sys
 import os
+from city.config import base_path
 config = {
     'host': '10.10.228.253',
     'user': 'mioji_admin',
@@ -14,33 +15,34 @@ config = {
     'charset': 'utf8'
 }
 
-conn = pymysql.connect(**config)
 
-def get_cityName():
+def get_cityName(config):
+    conn = pymysql.connect(**config)
     cursor = conn.cursor()
     select_sql = "select id,name,name_en from city where id=%s"
-    with open('city_id.csv','r+') as city:
+    with open(base_path+'city_id.csv','r+') as city:
         reader = csv.reader(city)
         next(reader)
-        with open('map_cityName.csv','w+') as name:
+        with open(base_path+'map_cityName.csv','w+') as name:
             writer = csv.writer(name)
             writer.writerow(('city_id','name','name_en'))
             for row in reader:
                 cursor.execute(select_sql,row)
                 result = cursor.fetchone()
                 writer.writerow(result)
-
-def revise_pictureNmae(path):
-    get_cityName()
+        conn.close()
+def revise_pictureNmae(path,config):
+    get_cityName(config)
     city_map = {}
-    with open('map_cityName.csv','r+') as city:
+    with open(base_path+'map_cityName.csv','r+') as city:
         reader = csv.reader(city)
         next(reader)
         for row in reader:
             city_map[row[1]] = row[0]
             city_map[row[2]] = row[0]
     file_names = os.listdir(path)
-    file_names.remove('.DS_Store')
+    if '.DS_Store' in file_names:
+        file_names.remove('.DS_Store')
     for file_name in file_names:
         real_name,extend_name = os.path.splitext(file_name)
         city_name = real_name.split('_')[0]
