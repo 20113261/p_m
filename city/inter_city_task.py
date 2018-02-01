c6191816c89d6f5b0ff6abca0911cfae84d85e83
luwanning <luwanning@mioji.com>
2018-02-01 20:53:11 +0800

    添加 发交通抓取 抓取平台需要的字段

--- city/inter_city_task.py
+++ city/inter_city_task.py
@@ -73,10 +73,14 @@ def city_pair(city_ids,config):
                     ))
                     continue
 
-                google_url = 'http://maps.google.cn/maps/api/directions/json?origin={}&destination={}&mode=driving'.format(
+                google_url = 'http://maps.google.cn/maps/api/directions/json?origin={}&destination={}&mode=driving&\
+                region=es&mode=driving&type=interCitya1={}&a2={}'.format(
                     src_map_info,
-                    dst_map_info
+                    dst_map_info,
+                    src_cid,
+                    dst_cid
                 )
+
                 logger.info("[new task][url: {}]".format(google_url))
                 pair.add((
                     src_cid,
