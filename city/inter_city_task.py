2140be6e4e03f99830e287924e70a3d717b3157c
luwanning <luwanning@mioji.com>
2018-02-03 21:25:30 +0800

    修改发任务错误  drive  <==>   not driver

--- city/inter_city_task.py
+++ city/inter_city_task.py
@@ -104,7 +104,7 @@ def google_driver(city_id,param,config,):
     task_name = 'google_driver_{0}_{1}'.format(param,time_tag)
 
 
-    with InsertTask(worker='proj.total_tasks.google_driver_task', queue='file_downloader', routine_key='file_downloader',
+    with InsertTask(worker='proj.total_tasks.google_drive_task', queue='file_downloader', routine_key='file_downloader',
                     task_name=task_name, source='Google', _type='GoogleDriveTask',
                     priority=11) as it:
         for s_cid, d_cid, google_url in res:
