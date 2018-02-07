6caa98c349930c4f96fff24360338701cf973914
vassago <f811194414@gmail.com>
2018-02-07 11:25:35 +0800

    ctrip poi mongo task query

---
+++ MongoTask/mongo_task_ctrip_poi_query.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @author: feng
+
+# @date: 2018-02-01
+from data_source import MysqlSource
+from logger import get_logger
+from MongoTask.MongoTaskInsert import InsertTask, TaskType
+from service_platform_conn_pool import source_info_config
+import pymongo
+logger = get_logger("insert_mongo_task")
+
+
+def get_tasks():
+    client = pymongo.MongoClient('mongodb://root:miaoji1109-=@10.19.2.103:27017/')
+    collections = client['SuggestName']['CtripPoiSDK']
+    tasks =set()
+    for data in collections.find():
+        try:
+            for d in data['suggest']['List']:
+                ur = d['Url'].split('/')[2]
+                tasks.add(ur.split('.')[0])
+                if len(tasks) == 10000:
+                    break
+        except:
+            pass
+        if len(tasks)==10000:
+            break
+    print(len(tasks))
+    return tasks
+
+
+if __name__ == '__main__':
+    with InsertTask(worker='proj.total_tasks.ctrip_poi_list_task', queue='poi_list', routine_key='poi_list',
+                    task_name='poi_total_ctripPoi_20180206c', source='CtripPoi', _type='CtripList',
+                    priority=3, task_type=TaskType.LIST_TASK) as it:
+        for line in get_tasks():
+            args = {
+                "city_id": "000",
+                "country_id": "000",
+                "source": "ctripPoi",
+                'city_url': line
+            }
+            it.insert_task(args)
