daebf0c91e960ed631068f9067697703bc7e25db
vonsago <vassagovon@vassagodeMacBook-Pro.local>
2018-02-08 09:48:36 +0800

    modify ctrip poi task

--- MongoTask/mongo_task_ctrip_poi_query.py
+++ MongoTask/mongo_task_ctrip_poi_query.py
@@ -5,35 +5,37 @@
 
 # @date: 2018-02-01
 from data_source import MysqlSource
-from logger import get_logger
 from MongoTask.MongoTaskInsert import InsertTask, TaskType
 from service_platform_conn_pool import source_info_config
 import pymongo
-logger = get_logger("insert_mongo_task")
 
 
 def get_tasks():
     client = pymongo.MongoClient('mongodb://root:miaoji1109-=@10.19.2.103:27017/')
-    collections = client['SuggestName']['CtripPoiSDK']
-    tasks =set()
-    for data in collections.find():
-        try:
-            for d in data['suggest']['List']:
-                ur = d['Url'].split('/')[2]
-                tasks.add(ur.split('.')[0])
-                if len(tasks) == 10000:
-                    break
-        except:
-            pass
-        if len(tasks)==10000:
-            break
-    print(len(tasks))
+    collections = client['SuggestName']['CtripPoiSDK_mioiji']
+    #tasks =set()
+    #for data in collections.find():
+    #    try:
+    #        for d in data['suggest']['List']:
+    #            ur = d['Url'].split('/')[2]
+    #            tasks.add(ur.split('.')[0])
+    #            if len(tasks) == 10:
+    #                break
+    #    except:
+    #        pass
+    #    if len(tasks)==10:
+    #        break
+    #print(len(tasks))
+    tasks = []
+    for co in collections.find({}):
+        tasks.append(co['task'])
+
     return tasks
 
 
 if __name__ == '__main__':
     with InsertTask(worker='proj.total_tasks.ctrip_poi_list_task', queue='poi_list', routine_key='poi_list',
-                    task_name='poi_total_ctripPoi_20180206c', source='CtripPoi', _type='CtripList',
+                    task_name='poi_total_ctripPoi_20180207i', source='CtripPoi', _type='CtripList',
                     priority=3, task_type=TaskType.LIST_TASK) as it:
         for line in get_tasks():
             args = {
