7f8fe0aa58c3e09ba77592ef3a8035be746b2cf2
vonsago <f811194414@gmail.com>
2018-03-06 17:19:22 +0800

    tuniu ctrip grouptravel query task

--- MongoTask/mongo_task_GT_query.py
+++ MongoTask/mongo_task_GT_query.py
@@ -12,13 +12,10 @@ import pymongo
 
 def get_tasks():
     client = pymongo.MongoClient('mongodb://root:miaoji1109-=@10.19.2.103:27017/')
-    collections = client['SuggestName']['CtripPoiSDK_Mioji_1']
+    collections = client['SuggestName']['CtripGTCity_Mioji']
     tasks = []
     for co in collections.find({}):
         tasks.append(co)
-        if len(tasks) == 10:
-            break
-
     return tasks
 
 
@@ -26,23 +23,27 @@ if __name__ == '__main__':
     #
     # ctrip or tuniu
     #
-    args = [{
-        "dept_info": {
-            "id": "1",
-            "name": "北京",
-            "name_en": "Beijing"
-        },
-        "dest_info": {
-            "id": "438",
-            "name": "巴厘岛",
-            "name_en": "bali"
-        },
-        "vacation_type": "grouptravel",
+    args = []
+    for task in get_tasks():
+        if task['len_id'] != 1:
+            continue
+        args.append({
+            "dept_info": {
+                "id": "1",
+                "name": "北京",
+                "name_en": "Beijing"
+            },
+            "dest_info": {
+                "id": task['city_id'],
+                "name": task['name'],
+                "name_en": 'tour'
+            },
+            "vacation_type": "grouptravel",
 
-        'source':'ctrip'
-    }]
+            'source':'ctrip'
+        })
     with InsertTask(worker='proj.total_tasks.GT_list_task', queue='poi_list', routine_key='poi_list',
-                    task_name='city_total_GT_20180305a', source='GT', _type='GTList',
+                    task_name='city_total_GT_20180306a', source='GT', _type='GTList',
                     priority=3, task_type=TaskType.CITY_TASK) as it:
         for line in args:
 
