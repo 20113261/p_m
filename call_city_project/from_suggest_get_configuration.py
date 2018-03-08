7d4b6922aabd8df1964fc5794d91571a293b9ca7
luwanning <luwanning@mioji.com>
2018-03-08 09:27:20 +0800

    修改上传为异步 添加上传进度 任务进度

--- call_city_project/from_suggest_get_configuration.py
+++ call_city_project/from_suggest_get_configuration.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from werkzeug.utils import secure_filename
-from flask import Flask, request, jsonify, send_from_directory, render_template, abort
+from flask import Flask, request, jsonify, send_from_directory, render_template, abort, send_file
 import os
 import base64
 import time
 import zipfile
 import pymysql
 import subprocess
+from urllib.parse import quote
+from io import BytesIO, StringIO
+import csv
 
+from call_city_project.monitor_status import monitor_step3
 from city.add_city import read_file
-from city.config import OpCity_config, base_path, config as temp_config
+from city.config import OpCity_config, data_config, config as temp_config
 from MongoTask.crawl_all_source_suggest import create_task
 from call_city_project.step_status import modify_status
 from my_logger import get_logger
@@ -22,6 +26,7 @@ app = Flask(__name__, static_folder=basedir)
 UPLOAD_FOLDER='/search/service/nginx/html/MioaPyApi/store/upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 logger = get_logger('log', UPLOAD_FOLDER)
+files = {'sign': '景点配置', 'hotel': '酒店配置'}
 
 @app.route('/')
 def main():
@@ -46,6 +51,7 @@ def upload_file():
         # zip_file_name = os.path.join(file_dir, new_filename)
         zip_file_name = os.path.join(file_dir, fname)
         f.save(zip_file_name)  # 保存文件到upload目录
+        print(zip_file_name)
         token = base64.b64encode(new_filename.encode('utf8'))
         logger.info('[upload][%s] token: %s' % (param, token))
         zip_path = os.path.join(file_dir, fname)
@@ -67,23 +73,26 @@ def upload_file():
         if not city_file:
             return jsonify({'status': False, 'message': '无新增城市配置文件'})
 
-        database_name = ''.join(['add_city_', param])
-        temp_config['db'] = database_name
-        collection_name, task_name = create_task(city_file, file_dir, database_name, param)
+        truncate_table()
+        collection_name, task_name = create_task(city_file, file_dir, 'BaseDataFinal', param)
         tasks = modify_status('step3', param, [collection_name, task_name])
-        from_ota_get_city(temp_config, param)
         logger.info('[upload][%s] tasks: %s' % (param, str(tasks)))
 
-        return jsonify({"errno": 0, "errmsg": "上传成功", "token": token})
+        return jsonify({"code": 0, "errmsg": "上传成功"})
     else:
-        return jsonify({"errno": 1001, "errmsg": "上传失败"})
+        return jsonify({"code": 1001, "errmsg": "上传失败"})
 
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     if request.method == "GET":
-        if os.path.isfile(os.path.join(UPLOAD_FOLDER+'/684/', filename+'.csv')):
-            return send_from_directory(UPLOAD_FOLDER+'/684/', filename+'.csv', as_attachment=True)
+        param = max(path for path in os.listdir(UPLOAD_FOLDER) if path.isdigit())
+        encodeurl_filename = quote(files[filename]+'.csv')
+        if os.path.isfile(os.path.join(UPLOAD_FOLDER+'/'+param+'/', files[filename]+'.csv')):
+            f = open(UPLOAD_FOLDER + '/' + param + '/' + files[filename] + '.csv', 'rb')
+            sf = send_file(f, as_attachment=True, attachment_filename=encodeurl_filename)
+            sf.headers['Content-Disposition'] += "; filename*=utf-8''{}".format(encodeurl_filename)
+            return sf
         abort(404)
 
 def get_param():
@@ -95,6 +104,19 @@ def get_param():
     cursor.close()
     conn.close()
     return param+1
+
+def truncate_table():
+    conn = pymysql.connect(**data_config)
+    cursor = conn.cursor()
+    sql = 'truncate table BaseDataFinal.ota_location;'
+    cursor.execute(sql, ())
+    cursor.close()
+    conn.close()
+
+@app.route('/taskprocess')
+def get_process():
+    process = monitor_step3('3')
+    return jsonify({'code': 0, 'process': process})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7698, debug=True)
