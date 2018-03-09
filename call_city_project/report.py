df4c7b7fc07873944345c83ac0ae082d5da06885
luwanning <luwanning@mioji.com>
2018-03-09 15:39:00 +0800

    修改第8步报表

--- call_city_project/report.py
+++ call_city_project/report.py
@@ -5,6 +5,8 @@ from city.config import base_path
 from os.path import join as path_join
 from os import system
 from my_logger import get_logger
+import pymysql
+from city.config import data_config
 
 poi_and_hotel_report_name = 'poi_and_hotel_report.csv'
 merge_image_and_content = 'merge_image_and_content.txt'
@@ -31,12 +33,66 @@ def make_poi_and_hotel_report(data, param):
 
     return str(param) +'/'+poi_and_hotel_report_name
 
+def get_content_image_report():
+    conn = pymysql.connect(**data_config)
+    cursor = conn.cursor()
+    sel_content_sql = "select source,count(*) from base_data.hotel_unid group by source;"
+    cursor.execute(sel_content_sql)
+    contents_title_report = """
+source,  total,   success,   failed
+"""
+    content_report = """{0},  {1},  {2},  {3}"""
+    for source, count in cursor.fetchall():
+        content = content_report.format(source, count, '100%', '0%') + '\n'
+        contents_title_report += content
+
+    sel_image_sql = "select img_list, first_img from base_data.hotel;"
+    cursor.execute(sel_image_sql)
+    all_count = cursor.rowcount
+    gte_10 = 0
+    lt_30 = 0
+    gt_30 = 0
+    zero = 0
+    list_and_imgisnull = 0
+    for img_list, f_img in cursor.fetchall():
+        if img_list in ('', 'NULL', None):
+            zero += 1
+            continue
+        list_total = len(img_list.split('|'))
+        if list_total > 30:
+            gt_30 += 1
+        elif list_total > 10:
+            lt_30 += 1
+        else:
+            gte_10 += 1
+
+        if f_img in ('', 'NULL', None):
+            list_and_imgisnull += 1
+
+    img_report = """
+酒店总数    无图片    1-10    11-30     >30     无首图
+{0}       {1}      {2}    {3}     {4}     {5}
+""".format(all_count, zero, gte_10, lt_30, gt_30, list_and_imgisnull)
+
+    cursor.close()
+    conn.close()
+
+    return contents_title_report + '\n\n' + img_report
+
 def make_image_content_report(t_all, t_done, t_failed, param):
     txtfile = path_join(base_path, param, merge_image_and_content)
     print(txtfile)
+
     with open(txtfile, 'w') as f:
-        data = '第 {0} 批 酒店总数 {1} 生成成功 {2} 生成失败 {3}'.format(param, t_all, format(t_done/t_all, '.0%'), format(t_failed/t_all, '.0%'))
-        f.write(data)
+        data = """
+第 {0} 批
+酒店总数 生成成功 生成失败
+{1}    {2}     {3}""".format(param, t_all, format(t_done/t_all, '.0%'), format(t_failed/t_all, '.0%'))
+        f.write(data+'\n')
+        content_and_image = get_content_image_report()
+        f.write(content_and_image)
+        logger.info('{0}, \n{1}'.format(param, content_and_image))
+
     cmd = "rsync -vI {0} 10.10.150.16::opcity/{1}".format(txtfile, param)
     logger.info('{0}, 上传命令 {1}'.format(param, cmd))
     status = system(cmd)
@@ -60,6 +116,6 @@ def get_file(param, filename):
         return False
 
 if __name__ == '__main__':
-    # make_image_content_report(19666, 19345, 321, '674')
+    make_image_content_report(19666, 19345, 321, '697')
     # make_poi_and_hotel_report('', '674')
-    get_file('674', 'merge_image_and_contenta.txt')
+    # get_file('674', 'merge_image_and_contenta.txt')
