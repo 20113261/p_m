72f137b7e477602bfabf3e081625e69764837500
luwanning <luwanning@mioji.com>
2018-03-10 17:09:14 +0800

    修改调用流程

--- call_city_project/add_new_city.sh
+++ call_city_project/add_new_city.sh
@@ -2,11 +2,12 @@
 
 echo "Hello world";
 
-#将zip 文件解压到指定文件夹
-rsync -a 10.10.150.16::opcity/ /search/service/nginx/html/MioaPyApi/store/opcity/
-#echo "第一个参数：$1";
-unzip -d /search/service/nginx/html/MioaPyApi/store/opcity/$3 $1
-
+if [ $3 != "website" ]; then
+    #将zip 文件解压到指定文件夹
+    rsync -a 10.10.150.16::opcity/ /search/service/nginx/html/MioaPyApi/store/opcity/
+    #echo "第一个参数：$1";
+    unzip -d /search/service/nginx/html/MioaPyApi/store/opcity/$3 $1
+fi
 #导出数据表
 mysqldump -h10.10.69.170 -ureader -pmiaoji1109 base_data city > /search/cuixiyi/city.sql
 mysqldump -h10.10.69.170 -ureader -pmiaoji1109 base_data airport > /search/cuixiyi/airport.sql
@@ -18,7 +19,9 @@ mysqldump -h10.10.230.206 -umioji_admin -pmioji1109 -d source_info ota_location 
 #mysqldump -h10.10.69.170 -ureader -pmiaoji1109 base_data station > /search/cuixiyi/station.sql
 #mysqldump -h10.10.69.170 -ureader -pmiaoji1109 base_data chat_shopping > /search/cuixiyi/chat_shopping.sql
 
-mysql -h10.10.230.206 -umioji_admin -pmioji1109 -e "create database $2"
+if [ $3 != "website" ]; then
+    mysql -h10.10.230.206 -umioji_admin -pmioji1109 -e "create database $2"
+fi
 
 #将数据表导入临时库
 mysql -h10.10.230.206 -umioji_admin -pmioji1109 $2 < /search/cuixiyi/city.sql
